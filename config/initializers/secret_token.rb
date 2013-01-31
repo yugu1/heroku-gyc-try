6ba384adb5e224513a0c550a7cf902b854efed6a
Satoshi <yugu1works@gmail.com>
2013-01-31 23:32:52 +0900

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HerokuYgcTry::Application.config.secret_token = '22cdd01a2b98dc2bd44e5501abfdd143178edc9d2ca47614adcc46b313a29c45fc2e5c4c45b9f2a485885fa405f56fbfe787651b833dec6da5d48b17eb5ee7cf'
